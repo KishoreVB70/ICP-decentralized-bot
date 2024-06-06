f0283b017f68ab92e8f8e3bb0e0fc9095f226d18
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-06 10:45:59 +0530

    added comments to the backend canister code

--- src/dfinity_js_backend/src/index.ts
+++ src/dfinity_js_backend/src/index.ts
@@ -10,16 +10,21 @@ import cors from "cors";
 type Message = {
   role: string;
   content: string;
+  // UUID
   id: string;
 };
 
+// In turn unused
 type BaseMessage = {
   role: string;
   content: string;
 };
 
+// Create a new conversation
 type ConversationPayload = { userIdentity: string };
 
+// Un used
+// Add a message to conversation
 type AddMessgeToConversationPayload = {
   userIdentity: string;
   conversationId: string;
@@ -27,11 +32,13 @@ type AddMessgeToConversationPayload = {
 };
 
 type Conversation = {
+  // UUID
   id: string;
-  // A type of a type
+  // Array of type messages
   conversation: Message[];
 };
 
+// Un used
 type ErrorMessage = { message: string };
 
 // Stores the course of the conversation
@@ -42,29 +49,38 @@ export default Server(() => {
   app.use(express.json());
   app.use(cors());
 
+  // Create a new conversation
   app.put("/conversation", (req: Request, res: Response) => {
+
+    // Requires the identity of the user
     const conversationPayload = req.body as ConversationPayload;
     if (!conversationPayload) {
       return res.status(400).json({ message: "Invalid conversation payload" });
     }
 
+    // The first of the conversation is the bot greeting the user
     const message = { ...systemMessage, id: uuidv4() };
     const conversation = { id: uuidv4(), conversation: [message] };
     userConversation.insert(conversationPayload.userIdentity, conversation);
 
+    // Return the actual conversation with the id and the userID
     return res.status(200).json({
       conversation,
+      // This is repetitive as the conversation itself would contain the ID
       id: conversation.id,
       initiator: conversationPayload.userIdentity,
     });
   });
 
+  // Obtain the conversation based on the userIdentity
   app.get("/conversation/:userIdentity", (req: Request, res: Response) => {
     const userIdentity = req.params.userIdentity;
     if (!userIdentity) {
+      // 404 -> not found
       return res.status(404).json({ message: "User Identity is required" });
     }
 
+    // User identity is the key to the conversation storage
     const conversation = userConversation.get(userIdentity);
     if ("None" in conversation) {
       return res
@@ -75,8 +91,12 @@ export default Server(() => {
     return res.status(200).json(conversation.Some);
   });
 
+  // Add a new message for the user
   app.post("/add/conversation", (req: Request, res: Response) => {
     const payload = req.body;
+    // No check if the body consist of a user identity
+
+    // Check if user has any conversation
     const conversation = userConversation.get(payload.userIdentity);
     if ("None" in conversation) {
       return res.status(404).json({
@@ -90,29 +110,41 @@ export default Server(() => {
       !payload.message?.content ||
       !payload.message?.role
     ) {
-      return res.status(400).json({ message: "Invild payload" });
+      // Client error
+      return res.status(400).json({ message: "Invaild payload" });
     }
 
+    // Create new message with details provided by the user
     const newMessage = {
       role: payload.message.role,
       content: payload.message.content,
       id: uuidv4(),
     };
 
+    // Obtain the messages array of the conversation
     const messages = conversation.Some.conversation;
+
+    // Create new array of old messages + the new message
     const updatedMessages = [...messages, newMessage];
+
+    // Add the message to the conversation
     const updatedConversation = {
       id: payload.conversationId,
       conversation: updatedMessages,
     };
 
+    // Add conversation into the storage
+    // Shouldn't we use somethign like update rather than just plain insert???
     userConversation.insert(payload.userIdentity, updatedConversation);
     return res.status(201).json(newMessage);
   });
 
+  // Delete a conversation
+  // Looks like anyone can delete any conversation if they know the identity of the other user
   app.delete("/conversation/:userIdentity", (req: Request, res: Response) => {
     const userIdentity = req.params.userIdentity;
 
+    // Remove the conversation based on the user identity, if it aint present, then return error
     const removedConversation = userConversation.remove(userIdentity);
 
     if ("None" in removedConversation) {
